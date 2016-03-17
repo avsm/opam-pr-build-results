e559113acdab0c2790139a86daeb6e409c993970
Anil Madhavapeddy <anil@recoil.org>
2016-03-17 20:36:21 +0000

    fix header

--- generate-pr.sh
+++ generate-pr.sh
@@ -21,6 +21,6 @@ REMOTEREPO=`curl -s https://api.github.com/repos/$REPO/pulls/$PR | jq -r .head.r
 git branch -D $PR || true
 git checkout -B $PR
 dockerfile-gen -o build -b origin,master -b https://github.com/${REMOTEREPO}.git,$HEADREF -c 4.02.3 $PACKAGES
-echo "# OPAM PR [$PR](https://github.com/ocaml/opam-repository/pulls/$PR)\n\n" > build/header.md
+echo "# OPAM PR [$PR](https://github.com/ocaml/opam-repository/pull/$PR)\n\n" > build/header.md
 (git add build && git commit -m 'sync' -a) || true
 git checkout master
